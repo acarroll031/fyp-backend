2151fae8f4a9a888a90783eea39b42ece6f76a2a
Adam Carroll <adam.carroll.2023@mumail.ie>
2025-11-20 18:37:40 +0000

    implemented lecturer registration and login endpoints

--- main.py
+++ main.py
@@ -4,11 +4,60 @@ from contextlib import asynccontextmanager
 from dataProcessing import convert_grades_to_students
 
 import pandas as pd
-from fastapi import FastAPI, Request, Response, Depends, UploadFile, File
+from fastapi import FastAPI, Request, Response, Depends, UploadFile, File, HTTPException, status
 import joblib
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
+
+from datetime import datetime, timedelta
+from typing import Optional
+import bcrypt
+import jwt # For the tokens
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+SECRET_KEY = "secret"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def get_password_hash(password: str) -> str:
+
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(pwd_bytes, salt)
+    return hashed.decode('utf-8')
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    password_byte_encoded = plain_password.encode('utf-8')
+    hashed_byte_encoded = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(password_byte_encoded, hashed_byte_encoded)
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except jwt.PyJWTError:
+        raise credentials_exception
+    return email
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -153,6 +202,59 @@ async def post_grades(
         raise e
     finally:
         connection.close()
+
+class LecturerCreate(BaseModel):
+    email: str
+    password: str
+    lecturer_name: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+@app.post("/register")
+def register_lecturer(lecturer: LecturerCreate):
+    connection = sqlite3.connect("fyp_database.db")
+    cursor = connection.cursor()
+
+    hashed_password = get_password_hash(lecturer.password)
+
+    try:
+        cursor.execute("""
+            INSERT INTO lecturers (email, password_hash, lecturer_name)
+            VALUES (?, ?, ?)
+        """, (lecturer.email, hashed_password, lecturer.lecturer_name)
+        )
+        connection.commit()
+    except sqlite3.IntegrityError:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    finally:
+        connection.close()
+
+    return {"message": "Lecturer registered successfully"}
+
+@app.post("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    connection = sqlite3.connect("fyp_database.db")
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+
+    cursor.execute(""" SELECT * FROM lecturers WHERE email = ?""", (form_data.username, ))
+    lecturer = cursor.fetchone()
+    connection.close()
+
+    if not lecturer or not verify_password(form_data.password, lecturer["password_hash"]):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": lecturer["email"]}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 
